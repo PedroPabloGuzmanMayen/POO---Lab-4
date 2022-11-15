2a6e4a6db235069c59fbab7f0a9e40114810c99c
Pedro Pablo Guzmán Mayen <guz22111@uvg.edu.gt>
2022-11-14 21:27:18 -0600

    Métodos principales agregados
    Se agregaron los métodos principales

--- Lab4/src/CarC.java
+++ Lab4/src/CarC.java
@@ -2,99 +2,113 @@ import java.util.ArrayList;
 
 public class CarC extends CarClass implements ICarC {
 
+	protected int volume;
+	protected boolean isOn;
+	protected float frequency;
+	protected ArrayList<Float> stations;
+	protected float station;
+	protected String carClass;
+	protected boolean isPhoneConnected;
+	protected ArrayList<String> contacts; 
+	
+	public CarC() {
+		station = (float) 0.0;
+		stations = new  ArrayList<Float>();
+		volume = 0;
+		
+	}
     @Override
     public boolean SwitchRadioONOFF() {
-
+        
         return false;
     }
 
     @Override
     public boolean getRadioStatus() {
- 
+        
         return false;
     }
 
     @Override
     public int getVolume() {
-      
-        return 0;
+        return volume;
     }
 
     @Override
     public void setVolume(int Volume) {
-  
+       this.volume = Volume;
         
     }
 
     @Override
     public float SwitchFrequency(float frequency) {
-
+        
         return 0;
     }
 
     @Override
     public void setStation(float Volume) {
-  
+        this.station = Volume;
         
     }
 
     @Override
     public float getStation() {
-
-        return 0;
+        
+        return station;
     }
 
     @Override
     public void SaveStation(float _station) {
-
         
+        stations.add(_station);
     }
 
     @Override
     public float SelectSpecificStation(int _StationIndex) {
-
+        
         return 0;
     }
 
     @Override
     public int getAudioRepType() {
-
+       
         return 0;
     }
 
     @Override
     public void setAudioRepType() {
-
+       
         
     }
 
     @Override
     public ArrayList<ISong> getAListOfSongs(int TypeOfAudioReproduction) {
-
+   
         return null;
     }
 
     @Override
     public void NextSong(int CurrentIndex, int TypeOfAudioReproduction) {
-
+        
         
     }
 
     @Override
     public void PrevSong(int CurrentIndex, int TypeOfAudioReproduction) {
-
+      
         
     }
 
     @Override
     public String PlaySong(int TypeOfAudioReproduction) {
-
+   
         return null;
     }
 
     @Override
     public boolean Connect_DisconnectPhone() {
-  
+       
         return false;
     }
 
@@ -106,7 +120,7 @@ public class CarC extends CarClass implements ICarC {
 
     @Override
     public ArrayList<String> getListofContacts() {
-
+    
         return null;
     }
 
@@ -119,25 +133,64 @@ public class CarC extends CarClass implements ICarC {
     @Override
     public String makeCall(String NameforCall) {
 
-        return null;
+        return "Inicio la llamada con" + NameforCall;
     }
 
     @Override
     public String PlaceCallOnHold() {
 
-        return null;
+        return "Dejaste la llamada en espera";
     }
 
     @Override
     public String endCall() {
 
-        return null;
+        return "La llamada finalizo";
     }
 
     @Override
     public String getWeatherForCast() {
-   
+    	
         return null;
-    } 
+    }
+
+	public String getCarClass() {
+		return carClass;
+	}
+
+	public void setCarClass(String carClass) {
+		this.carClass = carClass;
+	}
+
+	public float getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(float frequency) {
+		this.frequency = frequency;
+	}
+
+	public boolean isOn() {
+		return isOn;
+	}
+
+	public void setOn(boolean isOn) {
+		this.isOn = isOn;
+	}
+
+	public boolean isPhoneConnected() {
+		return isPhoneConnected;
+	}
+
+	public void setPhoneConnected(boolean isPhoneConnected) {
+		this.isPhoneConnected = isPhoneConnected;
+	}
+
+    public ArrayList<Float> getStations(){
+    	return stations;
+    }
+    public ArrayList<String> getContacts(){
+    	return contacts;
+    }
     
 }
