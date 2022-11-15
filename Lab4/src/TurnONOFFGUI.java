118d3e59e0df8499b4b49c5ddfe7f1304c98d1a3
Pedro Pablo Guzmán Mayen <guz22111@uvg.edu.gt>
2022-11-14 21:19:52 -0600

    GUI
    Se agregaron las primeras clases de la GUI

--- Lab4/src/TurnONOFFGUI.java
+++ Lab4/src/TurnONOFFGUI.java
@@ -1,4 +1,60 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class TurnONOFFGUI {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+public class TurnONOFFGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TurnONOFFGUI frame = new TurnONOFFGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TurnONOFFGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Mercedez Benz Radio");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
+		lblNewLabel.setBounds(90, 6, 269, 74);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Encender");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ChooseCarClassGUI choosecar = new ChooseCarClassGUI();
+			}
+		});
+		btnNewButton.setBounds(168, 145, 117, 29);
+		contentPane.add(btnNewButton);
+	}
 }
+
+
